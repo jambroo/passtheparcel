27731cb1cd3b891391c70bb2cfc055622d47d630
James Brooking <jambroo@gmail.com>
2017-10-24 16:47:21 +0100

    passing parcel workks.

--- lambda/pass_the_parcel.js
+++ lambda/pass_the_parcel.js
@@ -1,14 +1,56 @@
 'use strict';
 
+const aws = require('aws-sdk');
+const lambda = new aws.Lambda({
+  region: process.env.AWS_REGION
+});
+const https = require('https');
+
 exports.handler = (event, context, callback) => {
-    let body = event.body;
-    let body_obj = JSON.parse(body);
+  let body = event;
+  let status;
+  let i = (body.queryStringParameters) ? parseInt(body.queryStringParameters.i) : body.i;
 
-    console.log(body_obj);
+  if (body.forward === "1") {
+      console.log("FORWARDING TO "+process.env.NEXT+"?i="+i+"!")
+      let path = process.env.NEXT+"?i="+i;
+      https.get(path, function(res) {
+        callback(null, {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify({forward_url: process.env.NEXT})
+        });
+      }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+        context.done(null, 'FAILURE');
+      });
+  } else {
+    status = "CALLING_NEXT";
 
-    callback(null, {
-        statusCode: 200,
-        headers: {},
-        body: JSON.stringify({test: 1})
-    });
-};
+    console.log("PARCEL RECEIVED ("+i+")!")
+
+    i--;
+
+    if (i >= 0) {
+      lambda.invoke({
+        FunctionName: 'passTheParcel',
+        InvocationType: 'Event',
+        Payload: JSON.stringify({forward: "1", i}, null, 2)
+      }, function(error, data) {
+          callback(null, {
+              statusCode: 200,
+              headers: {},
+              body: JSON.stringify({status})
+          });
+      });
+    } else {
+      status = "DONE";
+
+      callback(null, {
+          statusCode: 200,
+          headers: {},
+          body: JSON.stringify({status})
+      });
+    }
+  }
+}
