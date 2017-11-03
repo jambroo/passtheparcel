5cd461c54eadb21e2726c380c48bc6ee995f5334
James Brooking <jambroo@gmail.com>
2017-11-03 23:55:49 +0000

    jumpstart now triggers process.

--- jumpstart.js
+++ jumpstart.js
@@ -1,20 +1,59 @@
-var net = require('net');
-var socket = net.Socket();
+'use strict'
 
-var host = process.argv[2];
-var educate = process.argv[3];
-var next = process.argv[4];
+let places;
 
-socket.connect({
-  port: 8080,
-  host: host
+try {
+  places = require('./places.json');
+} catch (e) {
+  console.log("ERROR: No places.json provided.");
+  process.exit(1);
+}
+
+const nodes = places.map((a) => {
+  return a.address;
 });
 
-var current_time = new Date().getTime();
-if (educate) {
-    socket.write('{"educate":"1","next":"' + next + '"}');
-} else {
-    socket.write('{"source":"START","time":"'+current_time+'"}');
-}
-socket.end();
+const nodes_friendly = places.map((a) => {
+  return a.name;
+});
 
+const starting_node = 0;
+const WAIT_TIME = 200;
+let now = new Date();
+
+if ((process.argv.length != 4) || (!Number.isInteger(parseInt(process.argv[3]))) || (nodes_friendly.indexOf(process.argv[2]) == -1)) {
+  console.log("ERROR: Invalid parameters. Should be \"node jumpstart.js <region> <skip_count>\".");
+  console.log(Number.isInteger(process.argv[3]));
+}
+
+const start_region = process.argv[2];
+const skip_count = parseInt(process.argv[3]);
+
+const request = require('request');
+
+const poll = (node, end) => {
+  request("https://" + nodes[node] + ".amazonaws.com/prod/passTheParcel?get=1", (error, response, body) => {
+    let result = JSON.parse(body);
+    let diff = parseInt(result.status)-now.getTime();
+
+    if (diff < 0) {
+      setTimeout(() => {
+        poll(node, end);
+      }, WAIT_TIME);
+    } else {
+      console.log(nodes_friendly[node], diff/1000);
+      if (end > 0) {
+        poll((node+1)%3, end-1);
+      }
+    }
+  });
+}
+
+request("https://" + nodes[starting_node] + ".amazonaws.com/prod/passTheParcel?i=" + skip_count, (error, response, body) => {
+  let result = JSON.parse(body);
+  if (result.status == "CALLING_NEXT") {
+    console.log("Starting at "+start_region);
+  }
+
+  poll((starting_node+1)%3, skip_count-1);
+});
